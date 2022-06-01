aeede65ee8a8be992bf620439bff23b59812e513
yoni13 <76271912+yoni13@users.noreply.github.com>
2022-06-01 20:11:06 +0800

    Delete 林某佑喜歡某個人之新分頁 directory

--- 林某佑喜歡某個人之新分頁/src/search.js
+++
@@ -1,8 +0,0 @@
-let searchbar= document.getElementById("searchbar");
-
-searchbar.onkeyup = function (e) {
-    if (e.key === 'Enter') {
-    event.preventDefault();
-    window.location.replace('https://google.com/search?q='+ searchbar.value);
-}
-}
