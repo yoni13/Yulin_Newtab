f5b6c2399d988a11dc914559fbc4d446dc92bfbd
yoni13 <76271912+yoni13@users.noreply.github.com>
2022-06-01 20:12:45 +0800

    0.0.0.8 update(chrome only
    0.0.0.8 update(chrome only

---
+++ 阿奕愛小婕(下雪新分頁/src/background.js
@@ -0,0 +1,24 @@
+// name_vla = 0; default
+// name_vla = 1 only change blue
+// name_vla = 2 only change red
+// name_vla = 3 change both
+
+
+
+
+chrome.runtime.onInstalled.addListener(async () => {
+  chrome.storage.sync.get("name_vla", function(items) {
+    if (!chrome.runtime.error) {
+      console.log(items.name_vla);
+      if (items.name_vla == undefined) {
+        chrome.storage.sync.set({name_vla: 0});
+      }
+    }
+  });
+    let url = chrome.runtime.getURL("public/setting.html");
+    let tab = await chrome.tabs.create({ url });
+  });
+chrome.action.onClicked.addListener(async () => {
+  let url = chrome.runtime.getURL("public/setting.html");
+  let tabcl = await chrome.tabs.create({ url:url });
+});
