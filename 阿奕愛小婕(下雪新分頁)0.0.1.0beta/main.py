81d4af26489ae8224a59a9aa2a3a691a2a049920
yoni13 <76271912+yoni13@users.noreply.github.com>
2022-06-12 01:46:19 +0800

    0.0.1.0
    0.0.1.0 chrome

--- 阿奕愛小婕(下雪新分頁)0.0.1.0beta/main.py
+++
@@ -1,6 +0,0 @@
-from flask import *
-app = Flask(__name__,static_folder='',template_folder='')
-@app.route('/')
-def root():
-    return render_template('/public/index.html')
-app.run(host = '0.0.0.0',port = 80, debug = True)
--- 阿奕愛小婕(下雪新分頁)0.0.1.0beta/src/settab.js
+++
@@ -1,8 +0,0 @@
-let ntab = document.getElementById("nametab");
-let ctab = document.getElementById("colortab");
-ntab.onclick = function(){
-    openCity(event, 'name')
-}
-ctab.onclick = function(){
-    openCity(event, 'color')
-}
