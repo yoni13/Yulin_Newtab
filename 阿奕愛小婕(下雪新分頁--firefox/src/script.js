9f5a1e28464e053c0dce181fdbc944b79c31d6ae
yoni13 <76271912+yoni13@users.noreply.github.com>
2022-07-05 12:58:44 +0800

    Delete 阿奕愛小婕(下雪新分頁--firefox directory

--- 阿奕愛小婕(下雪新分頁--firefox/src/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("main").onload=function(){
-    const element = document.getElementById("loading");
-    element.remove();
-    document.getElementById("main").style.opacity = "1";
-}
